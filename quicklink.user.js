373112fcee1e2093d722cd124633082195a1dd37
Kieran Hunt <kieran.hunt92@gmail.com>
2019-02-16 14:10:51 +0200

    Move to a @require for retrieving quicklink

--- quicklink.user.js
+++ quicklink.user.js
@@ -1,20 +1,16 @@
 // ==UserScript==
 // @name         quicklink
 // @namespace    casa.kieran
-// @version      0.2
+// @version      0.3
 // @description  Adds Google's quicklink to every page
 // @author       Kieran Hunt
 // @match        https://*/*
 // @match        http://*/*
-// @grant        none
+// @require      https://unpkg.com/quicklink
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var scriptElement = document.createElement("script")
-    scriptElement.type = "text/javascript";
-    scriptElement.src = "//unpkg.com/quicklink";
-    scriptElement.onload = () => quicklink();
-    document.body.appendChild(scriptElement);
+    quicklink();
 })();
